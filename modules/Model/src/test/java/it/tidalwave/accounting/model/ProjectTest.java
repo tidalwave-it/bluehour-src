6cc04eb293a193c6cab0b8a32df19648f02d1dde
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-03-10 14:29:18 +0100

    Added test.

---
+++ modules/Model/src/test/java/it/tidalwave/accounting/model/ProjectTest.java
@@ -0,0 +1,72 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueHour
+ * http://bluehour.tidalwave.it - hg clone https://bitbucket.org/tidalwave/bluehour-src
+ * %%
+ * Copyright (C) 2013 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.accounting.model;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.testng.annotations.Test;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class ProjectTest
+  {
+    @Test
+    public void toString_must_be_properly_computed()
+      {
+        final Address a1 = Address.builder().withStreet("Foo Bar rd 20")
+                                            .withCity("San Francisco")
+                                            .withZip("12345")
+                                            .withState("CA")
+                                            .withCountry("USA")
+                                            .create();
+        final Customer c1 = Customer.builder().withName("Acme Corp.")
+                                              .withVatNumber("1233455345")
+                                              .withBillingAddress(a1)
+                                              .create();
+        final Project p = Project.builder().withAmount(new Money(10500, "EUR"))
+                                            .withCustomer(c1)
+                                            .withName("Project 1")
+                                            .withDescription("description of project 1")
+                   //                         .withStartDate(null)
+                   //                         .withEndDate(null)
+                                            .withNotes("Notes for project 1")
+                                            .withNumber("1")
+                                            .withHourlyRate(new Money(43, "EUR"))
+                                            .create();
+        assertThat(p.toString(), is("Project(customer=Customer(name=Acme Corp., billingAddress="
+                                           + "Address(street=Foo Bar rd 20, city=San Francisco, "
+                                           + "state=CA, country=USA, zip=12345), vatNumber=1233455345), "
+                                           + "name=Project 1, number=1, description=description of project 1, "
+                                           + "notes=Notes for project 1, hourlyRate=43 EUR, amount=10500 EUR, "
+                                           + "startDate=null, endDate=null)"));
+      }
+  }
