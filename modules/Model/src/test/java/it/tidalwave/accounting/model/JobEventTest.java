d89b3020ffd6d6bdf4de207b6c4c12571906833a
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-03-10 14:56:43 +0100

    Added test.

---
+++ modules/Model/src/test/java/it/tidalwave/accounting/model/JobEventTest.java
@@ -0,0 +1,77 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueHour
+ * http://bluehour.tidalwave.it - hg clone https://bitbucket.org/tidalwave/bluehour-src
+ * %%
+ * Copyright (C) 2013 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.accounting.model;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.testng.annotations.Test;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class JobEventTest
+  {
+    @Test
+    public void toString_must_be_properly_computed()
+      {
+        final Address a1 = Address.builder().withStreet("Foo Bar rd 20")
+                                            .withCity("San Francisco")
+                                            .withZip("12345")
+                                            .withState("CA")
+                                            .withCountry("USA")
+                                            .create();
+        final Customer c1 = Customer.builder().withName("Acme Corp.")
+                                              .withVatNumber("1233455345")
+                                              .withBillingAddress(a1)
+                                              .create();
+        final Project p = Project.builder().withAmount(new Money(10500, "EUR"))
+                                            .withCustomer(c1)
+                                            .withName("Project 1")
+                                            .withDescription("description of project 1")
+                   //                         .withStartDate(null)
+                   //                         .withEndDate(null)
+                                            .withNotes("Notes for project 1")
+                                            .withNumber("1")
+                                            .withHourlyRate(new Money(43, "EUR"))
+                                            .create();
+        final JobEvent j1 = JobEvent.builder().withProject(p)
+                                              .withName("Consultancy")
+                                              .withDescription("Consultancy description")
+//                                              .withStartDateTime(null)
+//                                              .withEndDateTime(null)
+                                              .withRate(new Money(48, "EUR"))
+                                              .withEarnings(new Money(430, "EUR"))
+                                              .create();
+
+        assertThat(j1.toString(), is("JobEvent(name=Consultancy, description=Consultancy description, "
+                                             + "startDateTime=null, endDateTime=null, earnings=430 EUR, rate=48 EUR)"));
+      }
+  }
