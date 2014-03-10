1592402f52b39979b1589668978e687ce1e8373b
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-03-10 14:13:32 +0100

    Added test.

---
+++ modules/Model/src/test/java/it/tidalwave/accounting/model/MoneyTest.java
@@ -0,0 +1,54 @@
+/*
+ * #%L
+ * *********************************************************************************************************************
+ *
+ * blueHour
+ * http://bluehour.tidalwave.it - hg clone https://bitbucket.org/tidalwave/bluehour-src
+ * %%
+ * Copyright (C) 2013 - 2013 Tidalwave s.a.s. (http://tidalwave.it)
+ * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
+ * #L%
+ */
+package it.tidalwave.accounting.model;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.testng.annotations.Test;
+
+/***********************************************************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id$
+ *
+ **********************************************************************************************************************/
+public class MoneyTest
+  {
+    @Test
+    public void toString_must_be_properly_computed()
+      {
+        final Money m1 = new Money(4053, "EUR");
+        assertThat(m1.toString(), is("4053 EUR"));
+
+      }
+    @Test
+    public void toString_for_ZERO_must_be_properly_computed()
+      {
+        assertThat(Money.ZERO.toString(), is("0 EUR"));
+      }
+  }
