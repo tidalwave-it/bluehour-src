2b26832588ee8a57a257db5c2bea7f5ce8142fdd
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-03-31 20:32:44 +0200

    Clean up.

--- modules/JavaFXApplication/src/main/java/it/tidalwave/accounting/ui/impl/javafx/JavaFXApplicationPresentationDelegate.java
+++ modules/JavaFXApplication/src/main/java/it/tidalwave/accounting/ui/impl/javafx/JavaFXApplicationPresentationDelegate.java
@@ -27,17 +27,7 @@
  */
 package it.tidalwave.accounting.ui.impl.javafx;
 
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
 import java.io.IOException;
-import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-//import org.springframework.beans.factory.annotation.Configurable;
-//import it.tidalwave.role.ui.javafx.StackPaneSelector;
-import lombok.extern.slf4j.Slf4j;
 //import static it.tidalwave.role.ui.javafx.impl.JavaFXSafeComponentBuilder.*;
 
 /***********************************************************************************************************************
@@ -49,48 +39,8 @@ import lombok.extern.slf4j.Slf4j;
 //@Configurable @Slf4j
 public class JavaFXApplicationPresentationDelegate
   {
-//    @Inject @Nonnull
-//    private SiteOpenerPresentationControl siteOpenerPresentationControl;
-//
-//    @Inject @Nonnull
-//    private StackPaneSelector stackPaneSelector;
-//
-//    @Inject @Nonnull
-//    private ContentEditorPresentationControl contentEditorPresentationControl;
-//
-//    @Inject @Nonnull
-//    private JavaFXContentEditorPresentation contentEditorPresentation;
-//
-//    @FXML
-//    private Button btOpen;
-//
-//    @FXML
-//    private StackPane stackPane;
-//
-//    @FXML
-//    private SplitPane pnVerticalSplit;
-//
-//    @FXML
-//    private MenuItem openSiteMenu;
-
     public void initialize()
       throws IOException
       {
-//        stackPaneSelector.registerArea("editorArea", stackPane);
-//        stackPaneSelector.add("editorArea", structureEditorPresentation.getNode());
-//        stackPaneSelector.add("editorArea", contentEditorPresentation.getNode());
-//
-//        pnVerticalSplit.getItems().add(structurExplorerPresentation.getNode());
-//        pnVerticalSplit.getItems().add(contentExplorerPresentation.getNode());
-//
-//        // FIXME: controllers can't initialize in postconstruct
-//        // Too bad because with PAC+EventBus we'd get rid of the control interfaces
-//        contentEditorPresentationControl.initialize();
-//        contentExplorerPresentationControl.initialize();
-//        structureEditorPresentationControl.initialize();
-//        structureExplorerPresentationControl.initialize();
-//
-//        // FIXME: this should be delegated to other handlers, as already done for the Editors
-//        siteOpenerPresentationControl.initialize(createInstance(JavaFXSiteOpenerPresentation.class, this));
       }
   }
