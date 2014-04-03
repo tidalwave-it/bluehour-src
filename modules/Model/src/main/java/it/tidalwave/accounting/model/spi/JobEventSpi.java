f61c93c13f1b72abae2bcc525684844f59c67941
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-04-03 21:36:24 +0200

    ...

--- modules/Model/src/main/java/it/tidalwave/accounting/model/spi/JobEventSpi.java
+++ modules/Model/src/main/java/it/tidalwave/accounting/model/spi/JobEventSpi.java
@@ -1,21 +1,39 @@
 /*
  * #%L
+ * *********************************************************************************************************************
+ *
+ * blueHour
+ * http://bluehour.tidalwave.it - hg clone https://bitbucket.org/tidalwave/bluehour-src
  * %%
+ * Copyright (C) 2013 - 2014 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
+ * *********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * *********************************************************************************************************************
+ *
+ * $Id$
+ *
+ * *********************************************************************************************************************
  * #L%
- */
-
-package it.tidalwave.accounting.model.spi;
+ */package it.tidalwave.accounting.model.spi;
 
 import javax.annotation.Nonnull;
 import java.time.Duration;
-import java.time.LocalDateTime;
 import it.tidalwave.accounting.model.JobEvent;
 import it.tidalwave.accounting.model.Money;
 
 /***********************************************************************************************************************
  *
- * @author  fritz
+ * @author  Fabrizio Giudici
  * @version $Id$
  *
  **********************************************************************************************************************/
