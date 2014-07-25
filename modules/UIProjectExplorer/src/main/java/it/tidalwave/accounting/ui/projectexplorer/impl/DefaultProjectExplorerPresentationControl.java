abf5925a735591d8bc4b477f7400165d33f8b7bc
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-07-25 13:02:11 +0200

    ...

--- modules/UIProjectExplorer/src/main/java/it/tidalwave/accounting/ui/projectexplorer/impl/DefaultProjectExplorerPresentationControl.java
+++ modules/UIProjectExplorer/src/main/java/it/tidalwave/accounting/ui/projectexplorer/impl/DefaultProjectExplorerPresentationControl.java
@@ -67,20 +67,36 @@ public class DefaultProjectExplorerPresentationControl implements ProjectExplore
       {
       }
 
+    /*******************************************************************************************************************
+     *
+     * Reacts to the notification that a {@link Customer} has been selected by populating the presentation with
+     * his projects.
+     * 
+     * @param  event  the notification event
+     *
+     ******************************************************************************************************************/
     @VisibleForTesting void onCustomerSelectedEvent (final @Nonnull @ListensTo CustomerSelectedEvent event)
       {
         log.info("onCustomerSelectedEvent({})", event);
         presentation.populate(event.getCustomer().findProjects()
-                    .sorted(comparing(Project::getName))
-                    .map(project -> createPresentationModelFor(project))
-//                  .map(project -> project.as(Presentable).createPresentationModel())
-                    .collect(toCompositePresentationModel()));
+                                                 .sorted(comparing(Project::getName))
+                                                 .map(project -> createPresentationModelFor(project))
+                                                 .collect(toCompositePresentationModel()));
       }
     
+    /*******************************************************************************************************************
+     *
+     * Creates a {@link PresentationModel} for a {@link Project} injecting a {@link Selectable} role which fires a
+     * {@link ProjectSelectedEvent} on selection.
+     * 
+     * @param  customer     the {@code Project}
+     * @return              the {@code PresentationModel}
+     *
+     ******************************************************************************************************************/
     @Nonnull
     @VisibleForTesting PresentationModel createPresentationModelFor (final @Nonnull Project project)
       {
-        final Selectable selectable = () -> messageBus.publish(new ProjectSelectedEvent(project));
-        return project.as(Presentable).createPresentationModel(selectable);
+        final Selectable publishEventOnSelection = () -> messageBus.publish(new ProjectSelectedEvent(project));
+        return project.as(Presentable).createPresentationModel(publishEventOnSelection);
       }
   }
