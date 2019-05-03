1ddf21b2d94a12d2bc5a792fa4d1476c756707a9
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2019-05-03 18:06:09 +0200

    Updated copyright notice.

--- modules/UICommons/src/main/java/it/tidalwave/accounting/util/DefaultPreferencesHandler.java
+++ modules/UICommons/src/main/java/it/tidalwave/accounting/util/DefaultPreferencesHandler.java
@@ -5,7 +5,7 @@
  * NorthernWind - lightweight CMS
  * http://northernwind.tidalwave.it - hg clone https://bitbucket.org/tidalwave/northernwind-src
  * %%
- * Copyright (C) 2013 - 2017 Tidalwave s.a.s. (http://tidalwave.it)
+ * Copyright (C) 2013 - 2019 Tidalwave s.a.s. (http://tidalwave.it)
  * %%
  * *********************************************************************************************************************
  *
